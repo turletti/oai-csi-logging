5ac32ea72f03c77475bf49d6404d5c55091052a6
turletti <turletti@users.noreply.github.com>
2026-05-07 23:29:18 +0200

    Add initialization guard to prevent multiple thread creation

--- src/csi_rb_logging.c
+++ src/csi_rb_logging.c
@@ -1,5 +1,5 @@
 #include <stddef.h>
 #include "csi_rb_logging_external.h"
 
-int csi_rb_logging_enabled = 0;
-void (*csi_rb_logging_callback)(const void *, const void *, const void *, const void *) = NULL;
+extern int csi_rb_logging_enabled = 0;
+extern void (*csi_rb_logging_callback)(const void *, const void *, const void *, const void *) = NULL;
